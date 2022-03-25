8cef7ffd95a1e7e86cf22903e92a4579e6b7a7bb
jitendra dasari <djnakumar@gmail.com>
2022-03-25 19:07:25 +0530

    initiating frontend light

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+echo -e "\e[36m installing nginx \e[0m"
 yum install nginx -y
-
+echo -e "\e[36m downloading nginx content \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-
+echo -e "\e[36m cleanup old nginx content and extract new downloaded archive \e[0m"
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
