6c100d74d65d10f26f502024702e253a4c8a8b89
jitendra dasari <djnakumar@gmail.com>
2022-03-26 15:47:54 +0530

    functions with barces practice

---
+++ functions.sh
@@ -0,0 +1,10 @@
+#!/bin0/bash
+print_massage () {
+  echo hello
+  echo pining of functions
+  echo welcome ro functions
+  echo "First Argument in Function = $1"
+
+}
+print_massage DevOps
+  echo "first Argument in Main Script = $1"
