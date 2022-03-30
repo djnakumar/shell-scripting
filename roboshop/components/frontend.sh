34c2f96008fd87573292a491057a16aab6c66d8e
jitendra dasari <djnakumar@gmail.com>
2022-03-30 17:47:57 +0530

    customized cmd line & reduced repeated use of cmds.

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -18,29 +18,31 @@ if [ "$user_id" -ne 0 ]; then
   echo you should run your script as sudo or root user
   exit 1
 fi
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
 
 print "installing nginx"
-yum install nginx -y
+yum install nginx -y >>$LOG_FILE
 statcheck $?
 
 print "downloading nginx content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
 statcheck $?
 
 print "cleanup old nginx content"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >>$LOG_FILE
 statcheck $?
 
 cd /usr/share/nginx/html
 
 print "extracting archive"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* >>$LOG_FILE . && mv static/* . >>$LOG_FILE
 statcheck $?
 
 print "update roboshop configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
 statcheck $?
 
 print "starting nginx"
-systemctl restart nginx && systemctl enable nginx
+systemctl restart nginx >>$LOG_FILE && systemctl enable nginx >>$LOG_FILE
 statcheck $?
