2ec390a00eb4569457bc509746d7ce55f1822928
jitendra dasari <djnakumar@gmail.com>
2022-03-31 16:10:45 +0530

    app user = roboshop

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -23,7 +23,7 @@ rm -rf /home/${APP_USER}/catalogue &>>${LOG_FILE}
 statcheck $?
 
 print "extract app content"
-cd /home/${APP_USER} &>>${LOG_FILE} && unzip /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
+cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
 statcheck $?
 
 print "install app dependencies"
