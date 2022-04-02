01a76edc33a0b41d59663af47d32bea7eace317b
jitendra dasari <djnakumar@gmail.com>
2022-04-02 13:29:37 +0530

    mod

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -45,25 +45,26 @@ APP_SETUP() {
 }
 
 SERVICE_SETUP() {
-   print "fix app user permissions"
-   chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
-   statcheck $?
 
-   print "setup systemd file"
-   sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
-          -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
-          -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
-          -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
-          -e 's/CARTENDPOINT/cart.roboshop.internal/' \
-          -e 's/DBHOST/mysql.roboshop.internal/' \
-          /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
-   statcheck $?
+  print "fix app user permissions"
+  chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
+  statcheck $?
 
-   print "restart ${COMPONENT} services"
-   systemctl daemon-reload &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE} && systemctl enable ${COMPONENT} &>>${LOG_FILE}
-   statcheck $?
+  print "setup systemd file"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
+         -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
+         -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
+         -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
+         -e 's/CARTENDPOINT/cart.roboshop.internal/' \
+         -e 's/DBHOST/mysql.roboshop.internal/' \
+         /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
+  statcheck $?
 
-   SERVICE_SETUP
+  print "restart ${COMPONENT} services"
+  systemctl daemon-reload &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE} && systemctl enable ${COMPONENT} &>>${LOG_FILE}
+  statcheck $?
+
+  SERVICE_SETUP
 }
 
 NODEJS() {
