7a768df69f31bb028089ed8ce6a2c7bff102284b
jitendra dasari <djnakumar@gmail.com>
2022-06-27 11:41:47 +0530

    creating ec2 instance

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-Devops-Practice" | jq '.image[].imageid' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.image[].imageid' | sed -e 's/"//g')
 
 echo $AMI_ID
