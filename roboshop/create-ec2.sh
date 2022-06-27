f09d4b13ce6856552e3d13a16aa1a162481c10c2
jitendra dasari <djnakumar@gmail.com>
2022-06-27 11:21:39 +0530

    creating ec2 instance

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -3,4 +3,3 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=centos-7-Devops-practice" | jq '.image[].imageid' | sed -e 's/"//g')
 
 echo $AMI_ID
-aws ec2 run-instance --image-id ${AMI_ID} --instance-type t2.micro
